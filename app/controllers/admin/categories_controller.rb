a9eea49f59be75b25433ab7461769a46656ef2ea
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-07 17:45:04 -0800

    fixed a bug in new catagory button

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,11 +6,11 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-    @category.Category.new
+    @category = Category.new
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
